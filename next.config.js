ea3f50ed4203bbee6ca332885742f14ca0e499c9
Mark Nygaard <mark.nygaard@hotmail.com>
2022-04-24 20:29:45 +0200

    redirects from the old site added

--- next.config.js
+++ next.config.js
@@ -4,6 +4,25 @@ const nextConfig = {
   images: {
     domains: ['www.datocms-assets.com'],
   },
+  redirects: async () => {
+    return [
+      {
+        source: '/blog/2020/03/hue-dimmer-script-in-node-red/',
+        destination: '/blog/hue-dimmer-script-in-node-red/', // Matched parameters can be used in the destination
+        permanent: true,
+      },
+      {
+        source: '/blog/2019/09/building-a-magicmiror/',
+        destination: '/blog/building-a-magicmiror/', // Matched parameters can be used in the destination
+        permanent: true,
+      },
+      {
+        source: '/blog/2019/09/my-new-website-hosted-on-netlify/',
+        destination: '/blog/my-new-website-hosted-on-netlify/', // Matched parameters can be used in the destination
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
