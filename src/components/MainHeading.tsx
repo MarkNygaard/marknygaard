3196d5ddf55e4907cdb49beecb2f4d20f8a553dc
Mark Nygaard <mark.nygaard@hotmail.com>
2022-05-17 21:40:07 +0200

    Corrected prose classes for MainHeading

--- src/components/MainHeading.tsx
+++ src/components/MainHeading.tsx
@@ -5,7 +5,7 @@ export default function MainHeading({ record }) {
     <div>
       <h1>{record.title}</h1>
       <div
-        className="prose"
+        className="prose dark:prose-invert prose-h1:mb-1 prose-a:text-pine-600 hover:prose-a:text-pine-700 dark:prose-a:text-gray-500 dark:hover:prose-a:text-gray-400"
         dangerouslySetInnerHTML={{ __html: record.subtitle }}
       />
     </div>
