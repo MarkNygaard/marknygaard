dcac4bc37753e1f3ca2e1924a77f53080ae8c547
Mark Nygaard <mark.nygaard@hotmail.com>
2023-10-03 22:51:12 +0200

    Vercel analytics added to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import Footer from 'components/Footer';
 import Header from 'components/Header';
 import queryDatoCMS from 'infrastructure/queryDatoCms';
 import { AllPagesDocument } from 'infrastructure/generated/graphql';
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -32,6 +33,7 @@ export default async function RootLayout({
           </main>
           <Footer />
         </Providers>
+        <Analytics />
       </body>
     </html>
   );
