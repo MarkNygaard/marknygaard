1412785260df1c7ce2cbf788b8936699c9904333
Mark Nygaard <47065655+MarkNygaard@users.noreply.github.com>
2024-06-01 20:34:39 +0200

    feat: added skeleton for the main blog page (#1023)

--- src/app/[slug]/loading.tsx
+++ src/app/[slug]/loading.tsx
@@ -1,10 +1,20 @@
+'use client';
+
 import { Spinner } from '@ui/spinner';
+import { BlogPageSkeleton } from 'components/PageSkeletons';
+import { usePathname } from 'next/navigation';
 
 export default function Loading() {
-  return (
-    <div className='flex h-center w-full items-center justify-center text-xl dark:text-gray-400'>
-      <Spinner />
-      <p>Loading...</p>
-    </div>
-  );
+  const pathname = usePathname();
+
+  if (pathname === '/blog') {
+    return <BlogPageSkeleton />;
+  } else {
+    return (
+      <div className='flex h-center w-full items-center justify-center text-xl dark:text-gray-400'>
+        <Spinner />
+        <p>Loading...</p>
+      </div>
+    );
+  }
 }
---
+++ src/components/PageSkeletons.tsx
@@ -0,0 +1,12 @@
+import { Skeleton } from '@ui/skeleton';
+
+export function BlogPageSkeleton() {
+  return (
+    <div className='my-6 flex flex-col gap-6'>
+      <Skeleton className='h-24 sm:h-60 lg:h-[190px]' />
+      <Skeleton className='h-24 sm:h-60 lg:h-[190px]' />
+      <Skeleton className='h-24 sm:h-60 lg:h-[190px]' />
+      <Skeleton className='h-24 sm:h-60 lg:h-[190px]' />
+    </div>
+  );
+}
