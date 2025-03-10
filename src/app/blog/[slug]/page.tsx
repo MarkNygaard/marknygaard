a992a35affefd9b3be138379d018c0a5383305f0
Mark Nygaard <47065655+MarkNygaard@users.noreply.github.com>
2025-03-10 20:54:35 +0100

    feat: added cover image sizes for mobile and tablet (#1519)

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { toNextMetadata } from 'react-datocms';
-import { SRCImage } from 'react-datocms';
 import { Section } from '@Blocks/Section/Section';
 import SectionMenu from '@Blocks/Section/SectionMenu';
 import Comment from '@Primitives/Comment';
+import ResponsiveImage from '@Primitives/ResponsiveImage';
 // import ViewCounter from 'components/ViewCounter';
 import { format } from 'date-fns';
 import type { SectionRecord } from 'infrastructure/generated/graphql';
@@ -46,9 +46,17 @@ export default async function Page({ params }: Params) {
           </div>
         </div>
         <div className='relative flex aspect-[2/1.004] border-[1px] border-pine-300 dark:border-gray-800 sm:col-span-2'>
-          {data.post.coverImage?.responsiveImage && (
-            <SRCImage data={data.post.coverImage.responsiveImage} />
-          )}
+          {data.post.coverImage?.mobile390 &&
+            data.post.coverImage?.mobile430 &&
+            data.post.coverImage.tablet &&
+            data.post.coverImage.desktop && (
+              <ResponsiveImage
+                mobile390={data.post.coverImage.mobile390}
+                mobile430={data.post.coverImage.mobile430}
+                tablet={data.post.coverImage.tablet}
+                desktop={data.post.coverImage.desktop}
+              />
+            )}
         </div>
       </div>
       <div className='flex'>
---
+++ src/components/Primitives/ResponsiveImage.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { SRCImage } from 'react-datocms';
+
+type ResponsiveImageProps = {
+  mobile390: any;
+  mobile430: any;
+  tablet: any;
+  desktop: any;
+};
+
+export default function ResponsiveImages({
+  mobile390,
+  mobile430,
+  tablet,
+  desktop,
+}: ResponsiveImageProps) {
+  const [imageData, setImageData] = useState(mobile390);
+
+  useEffect(() => {
+    const updateImage = () => {
+      if (window.innerWidth <= 390) {
+        setImageData(mobile390);
+      } else if (window.innerWidth <= 430) {
+        setImageData(mobile430);
+      } else if (window.innerWidth <= 820) {
+        setImageData(tablet);
+      } else {
+        setImageData(desktop);
+      }
+    };
+
+    updateImage();
+    window.addEventListener('resize', updateImage);
+    return () => window.removeEventListener('resize', updateImage);
+  }, [mobile390, mobile430, tablet, desktop]);
+
+  return <SRCImage data={imageData} />;
+}
