9ac28b05bd1204449d48e8b76aa1b3e71e145ed7
Mark Nygaard <47065655+MarkNygaard@users.noreply.github.com>
2025-03-10 22:02:29 +0100

    refactor: simplified props passed to ResponsiveImage component (#1525)

--- src/components/Blocks/Featured/Featured.tsx
+++ src/components/Blocks/Featured/Featured.tsx
@@ -33,13 +33,7 @@ export default function FeaturedBlock({
             >
               <div className='flex h-full flex-col overflow-hidden rounded-lg border-[1px] border-pine-200 bg-pine-50 font-light translate-z-0 hover:border-pine-300 dark:border-gray-800 dark:bg-gray-900 dark:hover:border-gray-700'>
                 <div className='relative'>
-                  <ResponsiveImage
-                    mobile390={post.coverImage.mobile390}
-                    mobile430={post.coverImage.mobile430}
-                    tablet={post.coverImage.tablet}
-                    desktop={post.coverImage.desktop}
-                    priority
-                  />
+                  <ResponsiveImage coverImage={post.coverImage} priority />
                 </div>
                 <p className='p-4'>{post.title}</p>
               </div>
