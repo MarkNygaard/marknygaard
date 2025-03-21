9eb263f8ba2379962aa7134bcccc5a183441ff24
Mark Nygaard Leth <mark.nygaard@hotmail.com>
2025-03-21 17:22:33 +0100

    fix: only set priority for mobile images

--- src/components/Primitives/ResponsiveImage.tsx
+++ src/components/Primitives/ResponsiveImage.tsx
@@ -13,15 +13,15 @@ export default function ResponsiveImage({
     <>
       {/* Desktop Image */}
       <div className='hidden lg:block'>
-        <SRCImage data={coverImage.desktop} priority={priority} />
+        <SRCImage data={coverImage.desktop} />
       </div>
       {/* Tablet Image */}
       <div className='hidden md:block lg:hidden'>
-        <SRCImage data={coverImage.tablet} priority={priority} />
+        <SRCImage data={coverImage.tablet} />
       </div>
       {/* Mobile Image */}
       <div className='hidden sm:block md:hidden'>
-        <SRCImage data={coverImage.mobile430} priority={priority} />
+        <SRCImage data={coverImage.mobile430} />
       </div>
       {/* Small Mobile Image */}
       <div className='sm:hidden'>
