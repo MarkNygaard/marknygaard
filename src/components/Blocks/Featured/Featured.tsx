ce4af5078311b6bcbb41cfccf0c5a1fc6b473500
Mark Nygaard <47065655+MarkNygaard@users.noreply.github.com>
2025-03-21 18:25:24 +0100

    fix: changed some framer motion animations to css animations (#1552)

--- src/components/Blocks/Featured/Featured.tsx
+++ src/components/Blocks/Featured/Featured.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import ResponsiveImage from '@Primitives/ResponsiveImage';
 import { FeaturedRecord } from 'infrastructure/generated/graphql';
+import { cn } from 'lib/utils';
 import Link from 'next/link';
-
-import FeaturedAnimation from './FeaturedAnimation';
 
 export default function FeaturedBlock({
   fadeIn,
@@ -11,12 +10,12 @@ export default function FeaturedBlock({
   posts,
 }: FeaturedRecord) {
   return (
-    <FeaturedAnimation
-      fadeIn={fadeIn}
-      fadeInDelay={fadeInDelay}
-      className='py-4 pt-10'
+    <div
+      className={cn('py-4 pt-10', fadeIn && 'animate-fade-in-up opacity-0')}
+      style={fadeInDelay ? { animationDelay: `${fadeInDelay}s` } : undefined}
     >
       <div className='mb-6 text-3xl font-medium'>Featured Posts</div>
+
       <div className='mb-4 grid grid-cols-1 gap-1 md:grid-cols-3 md:gap-10'>
         {posts?.map((post) => (
           <Link
@@ -24,34 +23,23 @@ export default function FeaturedBlock({
             href={`/blog/${post.slug}`}
             className='mx-auto h-full w-fit pb-2 md:pb-0'
           >
-            <FeaturedAnimation
-              whileHover={{
-                y: -5,
-                boxShadow: '0 5px 10px -1px rgb(0 0 0 / 0.1)',
-              }}
-              className='h-full w-full rounded-xl'
-            >
+            <div className='h-full w-full rounded-xl transition-all duration-200 ease-in-out hover:-translate-y-1 hover:shadow-md'>
               <div className='flex h-full flex-col overflow-hidden rounded-lg border-[1px] border-pine-200 bg-pine-50 font-light translate-z-0 hover:border-pine-300 dark:border-gray-800 dark:bg-gray-900 dark:hover:border-gray-700'>
                 <ResponsiveImage coverImage={post.coverImage} />
                 <p className='p-4'>{post.title}</p>
               </div>
-            </FeaturedAnimation>
+            </div>
           </Link>
         ))}
       </div>
+
       <div className='w-40'>
         <Link href='/blog/'>
-          <FeaturedAnimation
-            whileHover={{
-              y: -2,
-              boxShadow: '0 5px 10px -1px rgb(0 0 0 / 0.1)',
-            }}
-            className='rounded-md border-[1px] border-pine-200 p-3 text-gray-500 hover:border-gray-800 hover:text-gray-800 dark:border-gray-800 dark:hover:border-gray-700 dark:hover:text-gray-300'
-          >
+          <div className='rounded-md border-[1px] border-pine-200 p-3 text-gray-500 transition-all duration-200 ease-in-out hover:-translate-y-0.5 hover:border-gray-800 hover:text-gray-800 hover:shadow-md dark:border-gray-800 dark:hover:border-gray-700 dark:hover:text-gray-300'>
             Read all posts -&gt;
-          </FeaturedAnimation>
+          </div>
         </Link>
       </div>
-    </FeaturedAnimation>
+    </div>
   );
 }
