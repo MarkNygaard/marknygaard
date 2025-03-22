1944daef82cbb0aeb89edf6546b7c3458648b7fc
Mark Nygaard Leth <mark.nygaard@hotmail.com>
2025-03-22 13:42:58 +0100

    style: corrections to mobilenav on standalone

--- src/components/Layouts/Navigation/MobileNavigation.tsx
+++ src/components/Layouts/Navigation/MobileNavigation.tsx
@@ -11,7 +11,7 @@ export default function MobileNavigation({
   allPages: PageRecord[];
 }) {
   return (
-    <nav className='container relative z-10 mx-auto px-4 md:hidden standalone:fixed standalone:mb-6 standalone:w-screen standalone:bg-gradient-to-r standalone:from-pine-400 standalone:via-pine-300 standalone:to-pine-200 standalone:pb-0 standalone:pt-10 dark:standalone:from-gray-900 dark:standalone:via-gray-900 dark:standalone:to-gray-900 standalone:md:relative standalone:md:max-w-none standalone:md:pt-0'>
+    <nav className='container relative z-10 mx-auto px-4 md:hidden standalone:fixed standalone:mb-4 standalone:w-screen standalone:bg-gradient-to-r standalone:from-pine-400 standalone:via-pine-300 standalone:to-pine-200 standalone:pb-0 standalone:pt-8 dark:standalone:from-gray-900 dark:standalone:via-gray-900 dark:standalone:to-gray-900 standalone:md:relative standalone:md:max-w-none standalone:md:pt-0'>
       <div className='my-4 -ml-3 flex flex-1 justify-between'>
         <MobileNavigationMenu allPages={allPages} />
         <div className='ml-5 flex space-x-3'>
