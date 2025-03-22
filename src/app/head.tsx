f8c911e064fa7afead1007fe6c91e3f641fcf425
Mark Nygaard Leth <mark.nygaard@hotmail.com>
2025-03-22 15:02:17 +0100

    style: removed black-translucent bar in standalone due to it not working

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -9,10 +9,6 @@ export default function Head() {
 
       {/* Web App Capabilities */}
       <meta name='apple-mobile-web-app-capable' content='yes' />
-      <meta
-        name='apple-mobile-web-app-status-bar-style'
-        content='black-translucent'
-      />
       <meta name='apple-mobile-web-app-title' content='Mark Nygaard' />
 
       {/* Manifest + Favicon */}
