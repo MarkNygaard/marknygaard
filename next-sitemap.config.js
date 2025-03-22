030f63885df7d4ffad3c85d8d4f3afb313998300
Mark Nygaard <47065655+MarkNygaard@users.noreply.github.com>
2025-03-22 13:15:22 +0100

    feat: added apple splash screens (#1562)

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,16 +3,4 @@
 module.exports = {
   siteUrl: 'https://marknygaard.dk',
   generateRobotsTxt: true,
-  robotsTxtOptions: {
-    policies: [
-      {
-        userAgent: 'Algolia Crawler',
-        allow: ['/sitemap.xml', '/sitemap-0.xml', '/blog/*'],
-      },
-      {
-        userAgent: 'Algolia Crawler',
-        disallow: ['/about', '/blog', '/'],
-      },
-    ],
-  },
 };
---
+++ src/app/head.tsx
@@ -0,0 +1,144 @@
+export default function Head() {
+  return (
+    <>
+      <meta name='viewport' content='width=device-width, initial-scale=1' />
+      <meta name='apple-mobile-web-app-capable' content='yes' />
+      <meta name='apple-mobile-web-app-status-bar-style' content='default' />
+      <meta name='apple-mobile-web-app-title' content='Mark Nygaard' />
+
+      <link rel='apple-touch-icon' sizes='180x180' href='/apple-icon-180.png' />
+
+      {/* Apple Splash Screens */}
+      <link
+        rel='apple-touch-startup-image'
+        href='/apple-splash-640-1136.jpg'
+        media='(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)'
+      />
+      <link
+        rel='apple-touch-startup-image'
+        href='/apple-splash-750-1334.jpg'
+        media='(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)'
+      />
+      <link
+        rel='apple-touch-startup-image'
+        href='/apple-splash-828-1792.jpg'
+        media='(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)'
+      />
+      <link
+        rel='apple-touch-startup-image'
+        href='/apple-splash-1125-2436.jpg'
+        media='(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)'
+      />
+      <link
+        rel='apple-touch-startup-image'
+        href='/apple-splash-1136-640.jpg'
+        media='(device-width: 568px) and (device-height: 320px) and (-webkit-device-pixel-ratio: 2)'
+      />
+      <link
+        rel='apple-touch-startup-image'
+        href='/apple-splash-1170-2532.jpg'
+        media='(device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3)'
+      />
+      <link
+        rel='apple-touch-startup-image'
+        href='/apple-splash-1242-2208.jpg'
+        media='(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3)'
+      />
+      <link
+        rel='apple-touch-startup-image'
+        href='/apple-splash-1242-2688.jpg'
+        media='(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)'
+      />
+      <link
+        rel='apple-touch-startup-image'
+        href='/apple-splash-1284-2778.jpg'
+        media='(device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3)'
+      />
+      <link
+        rel='apple-touch-startup-image'
+        href='/apple-splash-1334-750.jpg'
+        media='(device-width: 667px) and (device-height: 375px) and (-webkit-device-pixel-ratio: 2)'
+      />
+      <link
+        rel='apple-touch-startup-image'
+        href='/apple-splash-1536-2048.jpg'
+        media='(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)'
+      />
+      <link
+        rel='apple-touch-startup-image'
+        href='/apple-splash-1620-2160.jpg'
+        media='(device-width: 810px) and (device-height: 1080px) and (-webkit-device-pixel-ratio: 2)'
+      />
+      <link
+        rel='apple-touch-startup-image'
+        href='/apple-splash-1668-2224.jpg'
+        media='(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)'
+      />
+      <link
+        rel='apple-touch-startup-image'
+        href='/apple-splash-1668-2388.jpg'
+        media='(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)'
+      />
+      <link
+        rel='apple-touch-startup-image'
+        href='/apple-splash-1792-828.jpg'
+        media='(device-width: 896px) and (device-height: 414px) and (-webkit-device-pixel-ratio: 2)'
+      />
+      <link
+        rel='apple-touch-startup-image'
+        href='/apple-splash-2048-1536.jpg'
+        media='(device-width: 1024px) and (device-height: 768px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)'
+      />
+      <link
+        rel='apple-touch-startup-image'
+        href='/apple-splash-2048-2732.jpg'
+        media='(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)'
+      />
+      <link
+        rel='apple-touch-startup-image'
+        href='/apple-splash-2160-1620.jpg'
+        media='(device-width: 1080px) and (device-height: 810px) and (-webkit-device-pixel-ratio: 2)'
+      />
+      <link
+        rel='apple-touch-startup-image'
+        href='/apple-splash-2208-1242.jpg'
+        media='(device-width: 736px) and (device-height: 414px) and (-webkit-device-pixel-ratio: 3)'
+      />
+      <link
+        rel='apple-touch-startup-image'
+        href='/apple-splash-2224-1668.jpg'
+        media='(device-width: 1112px) and (device-height: 834px) and (-webkit-device-pixel-ratio: 2)'
+      />
+      <link
+        rel='apple-touch-startup-image'
+        href='/apple-splash-2388-1668.jpg'
+        media='(device-width: 1194px) and (device-height: 834px) and (-webkit-device-pixel-ratio: 2)'
+      />
+      <link
+        rel='apple-touch-startup-image'
+        href='/apple-splash-2436-1125.jpg'
+        media='(device-width: 812px) and (device-height: 375px) and (-webkit-device-pixel-ratio: 3)'
+      />
+      <link
+        rel='apple-touch-startup-image'
+        href='/apple-splash-2532-1170.jpg'
+        media='(device-width: 844px) and (device-height: 390px) and (-webkit-device-pixel-ratio: 3)'
+      />
+      <link
+        rel='apple-touch-startup-image'
+        href='/apple-splash-2688-1242.jpg'
+        media='(device-width: 896px) and (device-height: 414px) and (-webkit-device-pixel-ratio: 3)'
+      />
+      <link
+        rel='apple-touch-startup-image'
+        href='/apple-splash-2732-2048.jpg'
+        media='(device-width: 1366px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)'
+      />
+      <link
+        rel='apple-touch-startup-image'
+        href='/apple-splash-2778-1284.jpg'
+        media='(device-width: 926px) and (device-height: 428px) and (-webkit-device-pixel-ratio: 3)'
+      />
+    </>
+  );
+}
